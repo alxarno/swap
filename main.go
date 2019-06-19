aa506b92ddd1f95c046f27fdda2e1bb7fea05746
AlexeyArno <ledssssa@gmail.com>
2019-06-19 21:19:08 +0300

    Added gracefull shutdown

--- main.go
+++ main.go
@@ -2,13 +2,16 @@ package main
 
 import (
 	"bufio"
+	"context"
 	"crypto/rand"
 	"crypto/tls"
 	"net"
 	"net/http"
 	"os"
+	"os/signal"
 	"path/filepath"
 	"strings"
+	"time"
 
 	swapcrypto "github.com/alxarno/swap/crypto"
 	logger "github.com/alxarno/swap/logger"
@@ -17,23 +20,6 @@ import (
 	"github.com/alxarno/swap/settings"
 	engine "github.com/alxarno/swap/src/messages"
 )
-
-// type proveConnection struct {
-// 	Login string
-// 	Pass  string
-// }
-
-// type requestGetMessage struct {
-// 	Author string
-// 	ChatID int64
-// }
-
-// type errorAnswer struct {
-// 	Result string
-// 	Type   string
-// }
-
-// type client chan<- models.NewMessageToUser
 
 func removeContents(dir string) error {
 	d, err := os.Open(dir)
@@ -94,24 +80,56 @@ func main() {
 		}
 	}
 
-	printLogo()
-	os.Stderr.WriteString("Swap started on \t" + myAddres + "\n")
-	logger.Logger.Println("Swap started ...")
-
 	tlsconfig := tls.Config{Certificates: []tls.Certificate{*swapcrypto.Cert}}
 	tlsconfig.Rand = rand.Reader
-	server := http.Server{
+	tlsserver := http.Server{
 		TLSConfig: &tlsconfig,
 		Handler:   router,
 		Addr:      ":" + settings.ServiceSettings.Backend.SSLHost,
 	}
 
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+	r := http.NewServeMux()
+	r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		http.Redirect(w, r, "https://"+strings.Split(r.Host, ":")[0]+":"+settings.ServiceSettings.Backend.SSLHost+r.URL.String(), http.StatusPermanentRedirect)
 	})
 
-	go logger.Logger.Fatal(http.ListenAndServe(":"+settings.ServiceSettings.Backend.Host, nil))
+	httpserver := http.Server{
+		Addr:    ":" + settings.ServiceSettings.Backend.Host,
+		Handler: r,
+	}
 
-	logger.Logger.Fatal("ListenAndServeTLS: ", server.ListenAndServeTLS("", ""))
+	go func() {
+		if err := httpserver.ListenAndServe(); err != http.ErrServerClosed {
+			logger.Logger.Printf("Listen Error HTTP: %v\n", err)
+		}
+	}()
+
+	go func() {
+		if err := tlsserver.ListenAndServeTLS("", ""); err != http.ErrServerClosed {
+			logger.Logger.Printf("Listen Error TLS: %v\n", err)
+		}
+	}()
+
+	printLogo()
+	os.Stderr.WriteString("Swap started on \t" + myAddres + "\n")
+	logger.Logger.Println("Swap started")
+
+	stop := make(chan os.Signal, 1)
+	signal.Notify(stop, os.Interrupt)
+
+	<-stop
+
+	logger.Logger.Println("Swap shutdowned")
+
+	httpctx, httpcancel := context.WithTimeout(context.Background(), 5*time.Second)
+	httpsctx, httpscancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer httpcancel()
+	defer httpscancel()
+	if err = httpserver.Shutdown(httpctx); err != nil {
+		logger.Logger.Printf("Could not gracefully shutdown the http server %v\n", err)
+	}
+	if err = tlsserver.Shutdown(httpsctx); err != nil {
+		logger.Logger.Printf("Could not gracefully shutdown the https server %v\n", err)
+	}
 
 }
